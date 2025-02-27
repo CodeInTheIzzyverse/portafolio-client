531fdae5031644b912477378312cd42088a366d0
Isabela Bedoya Gaviria <dev.izzyverse@gmail.com>
2025-02-26 22:00:43 -0500

    fix languages 1

--- src/i18n.js
+++ src/i18n.js
@@ -2,9 +2,11 @@ import i18n from "i18next";
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
+import { resources } from "./locales";
 
-i18n.use(LanguageDetector).use(initReactI18next).use(Backend).init({
+i18n.use(LanguageDetector).use(initReactI18next).init({
     debug: true,
     fallbackLng: "en",
     returnObjects: true,
+    resources,
 });
---
+++ src/locales/index.js
@@ -0,0 +1,16 @@
+// locales/index.js
+import en from "./en/translation.json";
+import es from "./es/translation.json";
+import it from "./it/translation.json";
+
+export const resources = {
+    en: {
+        translation: en,
+    },
+    es: {
+        translation: es,
+    },
+    it: {
+        translation: it,
+    },
+};
